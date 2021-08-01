1b2941803eb6fbab2db5b86f41b794fe2f1a0f42
IsBebs <banangurkkungen@gmail.com>
2021-08-01 16:32:47 +0200

    the movement script is updated so that the player can jump and move in the x axis

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -11,20 +11,68 @@ public class PlayerMovement : MonoBehaviour
     float JumpHeight = 0.0f;
     [SerializeField]
     float JumpTime = 0.0f;
-    float VelocityX;
-    float gravity;
+    [SerializeField]
+    float VelocityX = 1f;
+    float gravity = 0f;
+    [SerializeField]
+    float fallMultiplier = 0f;
 
     Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        gravity = ((-2* JumpHeight)/(JumpTime*JumpTime));
+        rigidbody.velocity = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        gravity = ((-2 * JumpHeight) / (JumpTime * JumpTime));
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            MoveX(0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            MoveX(2);
+        }
+        else
+        {
+          
+            MoveX(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            MoveY(true);
+        }
+        else
+        {
+            MoveY(false);
+        }
+    }
+
+    public void MoveY(bool jump = false)
+    {
+        if (jump == true)
+        {
+            Debug.Log("jump");
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, (2 * JumpHeight) / JumpTime);
+        }
+        else
+        {
+            Debug.Log("fall");
+            if (rigidbody.velocity.y < 0)
+            {
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y + (gravity * fallMultiplier * Time.deltaTime));
+            }
+            else
+            {
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y + (gravity * Time.deltaTime));
+            }
+        }
         
     }
 
@@ -32,6 +80,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (direction == 0)
         {
+            Debug.Log("moveLeft");
             rigidbody.velocity = new Vector2(-VelocityX, rigidbody.velocity.y);
         }
         else if (direction == 1)
@@ -40,12 +89,8 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (direction == 2)
         {
+            Debug.Log("MoveRight");
             rigidbody.velocity = new Vector2(VelocityX, rigidbody.velocity.y);
         }
-    }
-
-    public void Jump()
-    {
-        rigidbody.velocity = new Vector2( rigidbody.velocity.x,(2 * JumpHeight) / JumpTime);
     }
 }
